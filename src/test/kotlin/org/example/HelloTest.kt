a74268119f3e0afad630071d80234a771ed8f7ff
anmir <andrei_mir@mail.ru>
2020-02-29 12:59:50 +0300

    add allure

--- src/test/kotlin/org/example/HelloTest.kt
+++ src/test/kotlin/org/example/HelloTest.kt
@@ -1,8 +1,19 @@
 package org.example
 
-import org.junit.Test
-import kotlin.test.assertEquals
+import io.qameta.allure.Feature
+import io.qameta.allure.Severity
+import io.qameta.allure.SeverityLevel
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
 
 class HelloTest {
+
+    @Test
+    @Feature("Some feature")
+    @Severity(SeverityLevel.CRITICAL)
+    fun testOutput() {
+        Assertions.assertTrue(true)
+    }
 
 }
